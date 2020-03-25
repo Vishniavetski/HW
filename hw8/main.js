17cae1b64d14666fa0241cd69ce04d3f1903c8f1
Vishniavetski <91vishny@mail.ru>
2020-03-25 11:22:17 +0200

    дз из 11 урока

--- hw8/main.js
+++ hw8/main.js
@@ -51,14 +51,32 @@ console.log (isPalindrome ('Шалаш'));
 
 // задание 4
 
-function areAnagrams(word, wordAnagrams) {
-    var word = word.toLowerCase();
-    var wordAnagrams = word.split().sort().join('');
+function areAnagrams(str1, str2) {
 
-    if (wordAnagrams == word) {
+    var firstStr = str1.toLowerCase().split('').sort().join('');
+    var secondStr = str2.toLowerCase().split('').sort().join('');
+    if (firstStr == secondStr) {
         return true;
     } else {
-        return false
+        return false;
     }
 }
-console.log (areAnagrams ('кот' , 'окт'));
+
+areAnagrams('кот', 'отк');
+areAnagrams('кот', 'атк');
+areAnagrams('кот', 'отко');
+
+// задание 5
+
+function divideArr(array, size) {
+    var newArr = [];
+
+    for (var i = 0; i < array.length; i += size) {
+        newArr.push(array.slice(i, i + size));
+    }
+
+    return newArr;
+}
+
+divideArr([1, 2, 3, 4], 2); // [[1, 2], [3, 4]]
+divideArr([1, 2, 3, 4, 5, 6, 7, 8], 3); // [[1, 2, 3], [4, 5, 6], [7, 8]]
